e4251281bf8a15658706405d4fc07ffb698ed1bf
Pritansh Chandra <pritanshchandra@gmail.com>
2019-09-27 01:56:13 +0530

    AboutApp Toolbar Tweaks

--- app/src/main/java/com/prytech/gitrepo/ui/AboutApp.java
+++ app/src/main/java/com/prytech/gitrepo/ui/AboutApp.java
@@ -6,11 +6,30 @@ import android.os.Bundle;
 
 import com.prytech.gitrepo.R;
 
+import java.util.Objects;
+
 public class AboutApp extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        this.finish();
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("About App");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
