d3f21499535adab29b802b5e530c0830f2ae3ef5
Pritansh Chandra <pritanshchandra@gmail.com>
2019-09-27 12:45:10 +0530

    Twitter and Github Intents

--- app/src/main/java/com/prytech/gitrepo/ui/AboutApp.java
+++ app/src/main/java/com/prytech/gitrepo/ui/AboutApp.java
@@ -2,13 +2,24 @@ package com.prytech.gitrepo.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.prytech.gitrepo.R;
 
 import java.util.Objects;
 
 public class AboutApp extends AppCompatActivity {
+
+    private MaterialButton rateApp;
+    private MaterialButton viewSourceCode;
+    private ExtendedFloatingActionButton twitterBtn;
 
     @Override
     public void onBackPressed() {
@@ -27,9 +38,60 @@ public class AboutApp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
 
+        rateApp = findViewById(R.id.rateApp);
+        viewSourceCode = findViewById(R.id.githubButton);
+        twitterBtn = findViewById(R.id.twitterButton);
+
         Objects.requireNonNull(getSupportActionBar()).setTitle("About App");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+        rateApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        viewSourceCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGithub();
+            }
+        });
+
+        twitterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openTwitter();
+            }
+        });
+
+    }
+
+    private void openTwitter()  {
+
+        String profileUrl = "https://twitter.com/chandrapritansh";
+        Intent launchTwitter;
+        try {
+            getApplicationContext().getPackageManager().getPackageInfo("com.twitter.android", 0);
+            launchTwitter = new Intent(Intent.ACTION_VIEW, Uri.parse(profileUrl));
+            startActivity(launchTwitter);
+        }
+        catch (Exception twitterHandler) {
+            Toast.makeText(this, "Twitter app not found, Opening in Browser", Toast.LENGTH_SHORT).show();
+            launchTwitter = new Intent(Intent.ACTION_VIEW, Uri.parse(profileUrl));
+            startActivity(launchTwitter);
+        }
+    }
+
+    private void openGithub() {
+
+        String repoUrl = "https://github.com/pritanshchandra/GitRepo";
+        Intent openGithub = new Intent(Intent.ACTION_VIEW);
+        openGithub.setData(Uri.parse(repoUrl));
+        startActivity(openGithub);
 
     }
 }
