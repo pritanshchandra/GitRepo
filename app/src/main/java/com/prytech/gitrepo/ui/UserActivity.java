5b9035e91ad3ab499b862e254b0ff7ab0d3c5bbd
Pritansh Chandra <pritanshchandra@gmail.com>
2019-09-27 10:42:37 +0530

    Minor UX Fixes

--- app/src/main/java/com/prytech/gitrepo/ui/UserActivity.java
+++ app/src/main/java/com/prytech/gitrepo/ui/UserActivity.java
@@ -2,7 +2,9 @@ package com.prytech.gitrepo.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,10 +12,12 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 import com.prytech.gitrepo.R;
 
@@ -44,22 +48,18 @@ public class UserActivity extends AppCompatActivity {
                 new MaterialAlertDialogBuilder(UserActivity.this)
                         .setMessage("Do you want to Exit App ?")
                         .setCancelable(false)
-
                         .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 finishAffinity();
                             }
                         })
-
                         .setNegativeButton("No", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-
                             }
                         })
                         .show();
-
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -73,22 +73,61 @@ public class UserActivity extends AppCompatActivity {
 
         userName = findViewById(R.id.userName);
         fetchUserData = findViewById(R.id.fetchButton);
+        ConstraintLayout constraintLayout = findViewById(R.id.parentLayout);
 
         fetchUserData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                hideKeyboard();
                 startGithubActivity();
+            }
+        });
+
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard();
+                userName.clearFocus();
+                userName.setError(null);
             }
         });
     }
 
     public void startGithubActivity() {
         String githubUserName = Objects.requireNonNull(userName.getEditText()).getText().toString().trim();
-        Intent githubActivity = new Intent(UserActivity.this, GithubActivity.class);
-        githubActivity.putExtra("Username", githubUserName);
-        startActivity(githubActivity);
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 
+        if(githubUserName.isEmpty())
+        {
+            userName.setError("Username Required");
+            Snackbar errorSnackbar = Snackbar.make(findViewById(android.R.id.content),
+                    "Please Enter Github Username to Proceed", Snackbar.LENGTH_SHORT);
+            errorSnackbar.setAction("Okay", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    userName.requestFocus();
+                }
+            });
+            errorSnackbar.show();
+        }
+
+        else {
+            userName.clearFocus();
+            Intent githubActivity = new Intent(UserActivity.this, GithubActivity.class);
+            githubActivity.putExtra("Username", githubUserName);
+            startActivity(githubActivity);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        }
+    }
+
+    private void hideKeyboard() {
+        View view = this.getCurrentFocus();
+        if(view!=null){
+            InputMethodManager inputMethodManager = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
+
+            assert inputMethodManager != null;
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
     @Override
